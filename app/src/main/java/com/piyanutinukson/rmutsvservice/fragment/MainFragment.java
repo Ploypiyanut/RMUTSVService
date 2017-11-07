a976f0741d2760917f9980df2b3c1c7038faf375
piyanut <inukson2538@gmail.com>
2017-11-07 13:58:26 +0700

    On click Listener Ok

--- app/src/main/java/com/piyanutinukson/rmutsvservice/fragment/MainFragment.java
+++ app/src/main/java/com/piyanutinukson/rmutsvservice/fragment/MainFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.piyanutinukson.rmutsvservice.R;
 
@@ -14,6 +16,39 @@ import com.piyanutinukson.rmutsvservice.R;
  */
 
 public class MainFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+      //  Register controller
+        registerController();
+
+
+    }   // Main Method
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.textRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Replace Fragment
+//                getActivity().getSupportFragmentManager()
+//                        .beginTransaction()
+//                        .replace(R.id.contentFragmentMain,new RegisterFragment())
+//                        .commit();
+
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.contentFragmentMain, new RegisterFragment())
+                        .addToBackStack(null).commit();
+
+
+                // onClick
+            }
+        });
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,
